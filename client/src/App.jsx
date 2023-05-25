79ac0c8c7b70a969b5bdafaf1b275f591fb0b4d8
MarsIfeanyi <marschalice@gmail.com>
2023-05-25 21:49:45 +0100

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Sidebar, Navbar } from "./components";
+import { Home, CreateCampaign, Profile, CampaignDetails } from "./pages";
+
+const App = () => {
+  return (
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row  ">
+      <div
+        className="sm:flex hidden mr-10 relative
+       "
+      >
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5 ">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+
+          <Route path="/profile" element={<Profile />} />
+
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
