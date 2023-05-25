a87bb796ad3bcacc4e9d2eeb13042a68bf9cca89
MarsIfeanyi <marschalice@gmail.com>
2023-05-25 21:49:30 +0100

    Initial Commit

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import App from "./App";
+import "./index.css";
+import { StateContextProvider } from "./context";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <ThirdwebProvider desiredChainId={ChainId.Sepolia}>
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </ThirdwebProvider>
+);
