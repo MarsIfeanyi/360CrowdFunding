825c3ef194b222a58fb9764b50e2119d36ea8f8d
MarsIfeanyi <marschalice@gmail.com>
2023-05-27 14:51:09 +0100

    Update file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,7 @@ import { Home, CreateCampaign, Profile, CampaignDetails } from "./pages";
 const App = () => {
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row  ">
-      <div
-        className="sm:flex hidden mr-10 relative
-       "
-      >
+      <div className="sm:flex hidden mr-10 relative">
         <Sidebar />
       </div>
 
