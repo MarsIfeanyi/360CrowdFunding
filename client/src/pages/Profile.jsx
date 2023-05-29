21f5b3607a4251441ae446966b2a3c65a7c9e59f
MarsIfeanyi <marschalice@gmail.com>
2023-05-29 22:11:02 +0100

    update file

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import DisplayCampaigns from "../components/DisplayCampaigns";
+import { useStateContext } from "../context";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+
+  const { address, contract, getUserCampaigns } = useStateContext();
+
+  // Hint: you can't use async function directly inside useEffect and since  getUserCampaigns is an async function we call it inside this function and then call this function in the useEffect
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    // checks if contract exists and then  getUserCampaigns
+    if (contract) fetchCampaigns();
+  }, [address, contract]);
+
+  return (
+    <div className="">
+      <DisplayCampaigns
+        title="All Campaigns"
+        isLoading={isLoading}
+        campaigns={campaigns}
+      />
+    </div>
+  );
 };
 
 export default Profile;
